1a1421aad0d87e0a169cb9e37891f8195ee759fa
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-09-30 15:52:59 -0600

    Add prework

---
+++ promises/script.js
@@ -0,0 +1,48 @@
+const testNum = num => {
+  return new Promise((resolve, reject) => {
+    if (num > 10) {
+      return resolve(num + ' is greater than ten good job');
+    } else {
+      return reject(num + ' is less than ten try again')
+    }
+  })
+}
+
+testNum(23)
+  .then(data => console.log(data))
+  .catch(error => console.log(error))
+
+
+const makeAllCaps = wordArray => {
+  return new Promise((resolve, reject) => {
+    let filteredWords = wordArray.filter(word => typeof word === 'number');
+    if (!filteredWords.length) {
+      const capatalizedWords = wordArray.map(word => word.toUpperCase())
+      resolve(capatalizedWords)
+    } else (
+      reject('only strings here buddy')
+    )
+  })
+}
+
+const sortWords = wordArray => {
+  return new Promise((resolve, reject) => {
+    if (wordArray) {
+      resolve(wordArray.sort())
+    } else {
+      reject('only strings in this town')
+    }
+  })
+}
+
+makeAllCaps(['shiss', 'yam', 'ketchup', 'mustard'])
+  .then(words => sortWords(words))
+  .then(sortedWords => console.log(sortedWords))
+  .catch(error => console.log(error))
+
+
+
+
+
+
+
