22039ff4c733b8bd38eedfd1a0b6a1faa4e39d3d
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-10-01 15:06:49 -0600

    finish dat prework

--- express-prework/server.js
+++ express-prework/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const sunset = 'http://en.es-static.us/upl/2013/09/sunrise-red-sea-Graham-Telford-e1489764712368.jpg'
 
 const urlLogger = (request, response, next) => {
   console.log('Request URL:', request.url);
@@ -19,7 +20,15 @@ app.get('/json', (request, response) => {
   response.status(200).json({"name": "JunkleStein"});
 });
 
+app.get('/sunsets', (request, response) => {
+  response.status(200).send(`<img src=${sunset} />`)
+})
+
 app.listen(3000, () => {
   console.log('Express intro running on localhost:3000');
 });
+
+app.use((request, response) => {
+  response.status(404).send("Better luck next time buddy");
+})
 
